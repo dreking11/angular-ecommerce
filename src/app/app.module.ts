b583fd132c71a713a65c7ae23d3d2993e726656b
Dre King <dreking11@gmail.com>
2022-02-23 15:21:50 -0500

    Search for product by Category hardcoded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,22 @@ import { AppComponent } from './app.component';
 import { ProductListsComponent } from './components/product-lists/product-lists.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListsComponent},
+  {path: 'category', component: ProductListsComponent},
+  {path: 'products', component: ProductListsComponent},
+  {path: '', redirectTo: '/products',pathMatch: 'full'},
+  {path: '**', redirectTo: '/products',pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductListsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
